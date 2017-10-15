6cf509e69389d2a04380c8864548f3dcba0aefa3
Andrei Miulescu <lusu777@gmail.com>
2017-10-15 18:30:42 +1100

    Standard formatting

--- RatingsData.js
+++ RatingsData.js
@@ -1,101 +1,99 @@
-import React, { AsyncStorage } from 'react-native';
+import { AsyncStorage } from 'react-native'
 
-const keyPrefix = '@RatingRequestData.';
-const eventCountKey = keyPrefix + 'positiveEventCount';
-const ratedTimestamp = keyPrefix + 'ratedTimestamp';
-const declinedTimestamp = keyPrefix + 'declinedTimestamp';
-const feedbackTimestamp = keyPrefix + 'feedbackTimestamp';
+const keyPrefix = '@RatingRequestData.'
+const eventCountKey = keyPrefix + 'positiveEventCount'
+const ratedTimestamp = keyPrefix + 'ratedTimestamp'
+const declinedTimestamp = keyPrefix + 'declinedTimestamp'
+const feedbackTimestamp = keyPrefix + 'feedbackTimestamp'
 
 /**
  * Private class that let's us interact with AsyncStorage on the device
  * @class
  */
 class RatingsData {
+  constructor () {
+    this.initialize()
+  }
 
-	constructor() {
-		this.initialize();
-	}
+  // Should only be used for debug purposes
+  async resetData () {
+    await AsyncStorage.removeItem(ratedTimestamp)
+    await AsyncStorage.removeItem(declinedTimestamp)
+    await AsyncStorage.removeItem(feedbackTimestamp)
+    await AsyncStorage.setItem(eventCountKey, '0')
+  }
 
-	// Should only be used for debug purposes
-	async resetData() {
-		await AsyncStorage.removeItem(ratedTimestamp);
-		await AsyncStorage.removeItem(declinedTimestamp);
-		await AsyncStorage.removeItem(feedbackTimestamp);
-		await AsyncStorage.setItem(eventCountKey, '0');
-	}
+  // Get current count of positive events
+  async getCount () {
+    try {
+      let countString = await AsyncStorage.getItem(eventCountKey)
+      return parseInt(countString, 10)
+    } catch (ex) {
+      console.warn('Couldn\'t retrieve positive events count. Error:', ex)
+    }
+  }
 
-	// Get current count of positive events
-	async getCount() {
-		try {
-			let countString = await AsyncStorage.getItem(eventCountKey);
-			return parseInt(countString, 10);
-		} catch (ex) {
-			console.warn('Couldn\'t retrieve positive events count. Error:', ex);
-		}
-	}
+  // Increment count of positive events
+  async incrementCount () {
+    try {
+      let currentCount = await this.getCount()
+      await AsyncStorage.setItem(eventCountKey, (currentCount + 1).toString())
 
-	// Increment count of positive events
-	async incrementCount() {
-		try {
-			let currentCount = await this.getCount();
-			await AsyncStorage.setItem(eventCountKey, (currentCount + 1).toString());
+      return currentCount + 1
+    } catch (ex) {
+      console.warn('Could not increment count. Error:', ex)
+    }
+  }
 
-			return currentCount + 1;
-		} catch (ex) {
-			console.warn('Could not increment count. Error:', ex);
-		}
-	}
+  async getActionTimestamps () {
+    try {
+      let timestamps = await AsyncStorage.multiGet([ratedTimestamp, declinedTimestamp, feedbackTimestamp])
 
-	async getActionTimestamps() {
-		try {
-			let timestamps = await AsyncStorage.multiGet([ratedTimestamp, declinedTimestamp, feedbackTimestamp]);
+      return timestamps
+    } catch (ex) {
+      console.warn('Could not retrieve rated or declined timestamps.', ex)
+    }
+  }
 
-			return timestamps;
-		} catch (ex) {
-			console.warn('Could not retrieve rated or declined timestamps.', ex);
-		}
-	}
+  async recordDecline () {
+    try {
+      await AsyncStorage.setItem(declinedTimestamp, Date.now().toString())
+    } catch (ex) {
+      console.warn('Couldn\'t set declined timestamp.', ex)
+    }
+  }
 
-	async recordDecline() {
-		try {
-			await AsyncStorage.setItem(declinedTimestamp, Date.now().toString());
-		} catch (ex) {
-			console.warn('Couldn\'t set declined timestamp.', ex);
-		}
-	}
+  async recordRated () {
+    try {
+      await AsyncStorage.setItem(ratedTimestamp, Date.now().toString())
+    } catch (ex) {
+      console.warn('Couldn\'t set rated timestamp.', ex)
+    }
+  }
 
-	async recordRated() {
-		try {
-			await AsyncStorage.setItem(ratedTimestamp, Date.now().toString());
-		} catch (ex) {
-			console.warn('Couldn\'t set rated timestamp.', ex);
-		}
-	}
+  async recordFeedback () {
+    try {
+      await AsyncStorage.setItem(feedbackTimestamp, Date.now().toString())
+    } catch (ex) {
+      console.warn('Couldn\'t set feedback timestamp.', ex)
+    }
+  }
 
-	async recordFeedback() {
-		try {
-			await AsyncStorage.setItem(feedbackTimestamp, Date.now().toString());
-		} catch (ex) {
-			console.warn('Couldn\'t set feedback timestamp.', ex);
-		}
-	}
+  // Initialize keys, if necessary
+  async initialize () {
+    try {
+      let keys = await AsyncStorage.getAllKeys()
 
-	// Initialize keys, if necessary
-	async initialize() {
-		try {
-			let keys = await AsyncStorage.getAllKeys();
-
-			if (!keys.some((key) => key === eventCountKey)) {
-				// TODO: I don't think this actually ever gets executed...
-				console.log('Initializing blank values...');
-				await AsyncStorage.setItem(eventCountKey, '0');
-			}
-		} catch (ex) {
-			// report error or maybe just initialize the values?
-			console.warn('Uh oh, something went wrong initializing values!', ex);
-		}
-	}
-
+      if (!keys.some((key) => key === eventCountKey)) {
+        // TODO: I don't think this actually ever gets executed...
+        console.log('Initializing blank values...')
+        await AsyncStorage.setItem(eventCountKey, '0')
+      }
+    } catch (ex) {
+      // report error or maybe just initialize the values?
+      console.warn('Uh oh, something went wrong initializing values!', ex)
+    }
+  }
 }
 
-export default new RatingsData();
+export default new RatingsData()
--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-import React, { Platform, Alert, Linking } from 'react-native';
+import { Platform, Alert, Linking } from 'react-native'
 
-import * as StoreReview from 'react-native-store-review';
-import RatingsData from './RatingsData';
+import * as StoreReview from 'react-native-store-review'
+import RatingsData from './RatingsData'
 
 const _config = {
-	title: 'Share Your Feedback',
-	ratePrompt: 'Would you mind taking a quick moment to leave us a positive review?',
-	initialQuestion: 'Do you like this app?',
-	feedbackPrompt: 'Would you like to tell us about it?',
-	appStoreId: null,
-	feedbackLabels: {
-		yes: 'Yes',
-		no: 'No'
-	},
-	actionLabels: {
-		decline: 'Don\'t ask again',
-		delay: 'Maybe later...',
-		feedback: 'Submit Feedback',
-		accept: 'Rate the App'
-	},
-	timingFunction: function(currentCount) {
-		return currentCount > 1 && (Math.log(currentCount) / Math.log(3)).toFixed(4) % 1 == 0;
-	}
-};
+  title: 'Share Your Feedback',
+  ratePrompt: 'Would you mind taking a quick moment to leave us a positive review?',
+  initialQuestion: 'Do you like this app?',
+  feedbackPrompt: 'Would you like to tell us about it?',
+  appStoreId: null,
+  feedbackLabels: {
+    yes: 'Yes',
+    no: 'No'
+  },
+  actionLabels: {
+    decline: 'Don\'t ask again',
+    delay: 'Maybe later...',
+    feedback: 'Submit Feedback',
+    accept: 'Rate the App'
+  },
+  timingFunction: function (currentCount) {
+    return currentCount > 1 && (Math.log(currentCount) / Math.log(3)).toFixed(4) % 1 === 0
+  }
+}
 
-async function _isAwaitingRating() {
-	let timestamps = await RatingsData.getActionTimestamps();
+async function _isAwaitingRating () {
+  let timestamps = await RatingsData.getActionTimestamps()
 
-	// If no timestamps have been set yet we are still awaiting the user, return true
-	return timestamps.every((timestamp) => { return timestamp[1] === null; });
+  // If no timestamps have been set yet we are still awaiting the user, return true
+  return timestamps.every((timestamp) => { return timestamp[1] === null })
 }
 
 /**
@@ -36,113 +36,116 @@ async function _isAwaitingRating() {
  * @class
  */
 export default class RatingRequestor {
+  /**
+   * @param  {string} appStoreId - Required. The ID used in the app's respective app store
+   * @param  {object} options - Optional. Override the defaults. Takes the following shape, with all elements being optional:
+   *                 {
+   *                   title: {string},
+   *                   ratePrompt: {string},
+   *                   initialQuestion: {string},
+   *                   feedbackPrompt: {string},
+   *                   actionLabels: {
+   *                     decline: {string},
+   *                     delay: {string},
+   *                     accept: {string}
+   *                     feedback: {string}
+   *                   },
+   *                   timingFunction: {func}
+   *                 }
+   */
+  constructor (appStoreId, options) {
+    // Check for required options
+    if (!appStoreId) {
+      throw new Error('You must specify your app\'s store ID on construction to use the Rating Requestor.')
+    }
 
-	/**
-	 * @param  {string} appStoreId - Required. The ID used in the app's respective app store
-	 * @param  {object} options - Optional. Override the defaults. Takes the following shape, with all elements being optional:
-	 * 								{
-	 * 									title: {string},
-	 * 									ratePrompt: {string},
-	 * 									initialQuestion: {string},
-	 * 									feedbackPrompt: {string},
-	 * 									actionLabels: {
-	 * 										decline: {string},
-	 * 										delay: {string},
-	 * 										accept: {string}
-	 * 										feedback: {string}
-	 * 									},
-	 * 									timingFunction: {func}
-	 * 								}
-	 */
-	constructor(appStoreId, options) {
-		// Check for required options
-		if (!appStoreId) {
-			throw 'You must specify your app\'s store ID on construction to use the Rating Requestor.';
-		}
+    // Merge defaults with user-supplied config
+    Object.assign(_config, options)
+    _config.appStoreId = appStoreId
+  }
 
-		// Merge defaults with user-supplied config
-		Object.assign(_config, options);
-		_config.appStoreId = appStoreId;
-	}
+  /**
+   * For debug purposes
+   */
+  resetData () {
+    RatingsData.resetData()
+  }
 
-	/**
-	 * For debug purposes
-	 */
-	resetData() {
-		RatingsData.resetData();
-	}
+  recordFeedback () {
+    RatingsData.recordFeedback()
+  }
 
-	recordFeedback() {
-		RatingsData.recordFeedback();
-	}
+  /**
+   * Immediately invoke the store review
+   */
+  storeReview (cbFunction = () => { }) {
+    let storeUrl = Platform.OS === 'ios'
+      ? 'https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=' + _config.appStoreId + '&pageNumber=0&sortOrdering=2&type=Purple+Software&mt=8'
+      : 'market://details?id=' + _config.appStoreId
 
-	/**
-	 * Immediately invoke the store review
-	 */
-	storeReview(callback = () => {}) {
-		let storeUrl = Platform.OS === 'ios' ?
-			'https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=' + _config.appStoreId + '&pageNumber=0&sortOrdering=2&type=Purple+Software&mt=8' :
-			'market://details?id=' + _config.appStoreId;
+    RatingsData.recordRated()
+    cbFunction(true, 'accept')
 
-		RatingsData.recordRated();
-		callback(true, 'accept');
+    // This API is only available on iOS 10.3 or later
+    if (Platform.OS === 'ios' && StoreReview.isAvailable) {
+      StoreReview.requestReview()
+    } else {
+      Linking.openURL(storeUrl)
+    }
+  }
 
-		// This API is only available on iOS 10.3 or later
-		if (Platform.OS === 'ios' && StoreReview.isAvailable) {
-			StoreReview.requestReview();
-		} else {
-			Linking.openURL(storeUrl);
-		}
-	}
+  showFeedbackDialog (cbFunction = () => { }) {
+    Alert.alert(
+      _config.title,
+      _config.feedbackPrompt,
+      [
+        { text: _config.actionLabels.decline, onPress: () => { RatingsData.recordDecline(); cbFunction(true, 'decline') } },
+        { text: _config.actionLabels.delay, onPress: () => { cbFunction(true, 'delay') } },
+        { text: _config.actionLabels.feedback, onPress: () => { cbFunction(true, 'feedback') }, style: 'cancel' }
+      ]
+    )
+  }
 
-	showFeedbackDialog(callback = () => {}) {
-		Alert.alert(
-			_config.title,
-			_config.feedbackPrompt,
-			[
-				{ text: _config.actionLabels.decline, onPress: () => { RatingsData.recordDecline(); callback(true, 'decline'); } },
-				{ text: _config.actionLabels.delay, onPress: () => { callback(true, 'delay'); } },
-				{ text: _config.actionLabels.feedback, onPress: () => { callback(true, 'feedback'); }, style: 'cancel' }
-			]
-		);
-	}
+  showRatingDialog (cbFunction = () => { }) {
+    Alert.alert(
+      _config.title,
+      _config.ratePrompt,
+      [
+        { text: _config.actionLabels.decline, onPress: () => { RatingsData.recordDecline(); cbFunction(true, 'decline') } },
+        { text: _config.actionLabels.delay, onPress: () => { cbFunction(true, 'delay') } },
+        { text: _config.actionLabels.accept, onPress: () => this.storeReview(), style: 'cancel' }
+      ]
+    )
+  }
 
-	showRatingDialog(callback = () => {}) {
-		Alert.alert(
-			_config.title,
-			_config.ratePrompt,
-			[
-				{ text: _config.actionLabels.decline, onPress: () => { RatingsData.recordDecline(); callback(true, 'decline'); } },
-				{ text: _config.actionLabels.delay, onPress: () => { callback(true, 'delay'); } },
-				{ text: _config.actionLabels.accept, onPress: () => this.storeReview(), style: 'cancel' }
-			]
-		);
-	}
+  showInitialDialog (cbFunction = () => { }) {
+    Alert.alert(
+      _config.title,
+      _config.initialQuestion,
+      [
+        { text: _config.feedbackLabels.no, onPress: () => this.showFeedbackDialog(cbFunction) },
+        { text: _config.feedbackLabels.yes, onPress: () => this.showRatingDialog(cbFunction), style: 'cancel' }
+      ]
+    )
+  }
 
-	showInitialDialog(callback = () => {}) {
-		Alert.alert(
-			_config.title,
-			_config.initialQuestion,
-			[
-				{ text: _config.feedbackLabels.no, onPress: () => this.showFeedbackDialog(callback) },
-				{ text: _config.feedbackLabels.yes, onPress: () => this.showRatingDialog(callback), style: 'cancel' }
-			]
-		);
-	}
+  /**
+   * Call when a positive interaction has occurred within your application. Depending on the number
+   * of times this has occurred and your timing function, this may display a rating request dialog.
+   *
+   * @param {function(didAppear: boolean, result: string)} cbFunction Optional. cbFunction that reports whether the dialog appeared and what the result was.
+   */
+  async handlePositiveEvent (cbFunction = () => { }) {
+    if (await _isAwaitingRating()) {
+      let currentCount = await RatingsData.incrementCount()
 
-	/**
-	 * Call when a positive interaction has occurred within your application. Depending on the number
-	 * of times this has occurred and your timing function, this may display a rating request dialog.
-	 *
-	 * @param {function(didAppear: boolean, result: string)} callback Optional. Callback that reports whether the dialog appeared and what the result was.
-	 */
-	async handlePositiveEvent(callback = () => {}) {
-		if (await _isAwaitingRating()) {
-			let currentCount = await RatingsData.incrementCount();
-
-			if (_config.timingFunction(currentCount)) {
-				this.showInitialDialog(callback);
-			} else callback(false);
-		} else callback(false);
-	}
+      if (_config.timingFunction(currentCount)) {
+        this.showInitialDialog(cbFunction)
+      } else {
+        cbFunction(false)
+      }
+    } else {
+      cbFunction(false)
+    }
+  }
 }
